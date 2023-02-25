be550eaca84d15fcea7fc244f4ce194f2026b8d8
nhatduc <ducdn21102001@gmail.com>
2023-02-25 23:02:14 +0700

    createBarHealthForEnemy2

--- Assets/Scripts/Enemy/Enemy2.cs
+++ Assets/Scripts/Enemy/Enemy2.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Enemy2 : MonoBehaviour
 {
     private Animator animator;
-    int currentHealth;
+    public static float currentHealth;
+    public GameObject healthBar;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        currentHealth = animator.GetInteger("Health");
-
+        currentHealth = animator.GetFloat("Health");
     }
 
     void Update()
@@ -22,17 +22,17 @@ public class Enemy2 : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            Instantiate(healthBar, new Vector2(transform.position.x, transform.position.y + 0.7f), Quaternion.identity);
             Quaternion rotation = collision.gameObject.transform.rotation;
             gameObject.transform.rotation = rotation;
-            currentHealth -=10;
+            currentHealth -=0.05f;
             Debug.Log(currentHealth);
-            animator.SetInteger("Health", currentHealth);
+            animator.SetFloat("Health", currentHealth);
             animator.SetBool("IsAttack", true);
             if (currentHealth <= 0)
             {
                 Destroy(gameObject, 3f);
             }
-
         }
 
     }
