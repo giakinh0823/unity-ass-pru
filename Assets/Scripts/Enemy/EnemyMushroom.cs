f4b0908395d1922cdf865dc9d63eee60e76c3384
Ha Gia Kinh <giakinh2000@gmail.com>
2023-02-26 16:52:10 +0700

    fix enemy

--- Assets/Scripts/Enemy/EnemyMushroom.cs
+++ Assets/Scripts/Enemy/EnemyMushroom.cs
@@ -2,38 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy2 : MonoBehaviour
+public class EnemyMushroom : MonoBehaviour
 {
     private Animator animator;
     private float damage = 0.05f;
     public static float currentHealth = 1f;
     public GameObject healthBar;
+    public Healbar healbar;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.SetFloat("Health", currentHealth);
+        healbar = healthBar.gameObject.GetComponent<Healbar>();
     }
 
-    void Update()
+    private void Update()
     {
+        healbar.localScale.x = currentHealth;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            healthBar = Instantiate(healthBar, new Vector2(transform.position.x, transform.position.y + 0.7f), Quaternion.identity);
+            healthBar.SetActive(true);
             Quaternion rotation = collision.gameObject.transform.rotation;
             gameObject.transform.rotation = rotation;
             currentHealth -= damage;
-            Debug.Log(currentHealth);
+           
+
             animator.SetFloat("Health", currentHealth);
             animator.SetBool("IsAttack", true);
+            
             if (currentHealth <= 0)
             {
-                Destroy(healthBar);
-                Destroy(gameObject, 3f);
+                Destroy(gameObject, 2f);
             }
         }
 
