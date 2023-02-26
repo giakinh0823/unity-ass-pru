6cc212175cd03534887c2b5461d570b92801ff82
Ha Gia Kinh <giakinh2000@gmail.com>
2023-02-26 16:52:10 +0700

    fix enemy

--- Assets/Scripts/Enemy/Healbar.cs
+++ Assets/Scripts/Enemy/Healbar.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Healbar2 : MonoBehaviour
+public class Healbar : MonoBehaviour
 {
-    Vector3 localScale;
+    public Vector3 localScale;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,6 @@ public class Healbar2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        localScale.x = Enemy2.currentHealth;
         transform.localScale = localScale;
     }
 }
