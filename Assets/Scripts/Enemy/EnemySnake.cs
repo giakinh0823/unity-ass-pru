1048fa171b4a836a26a5a13d9c677afaff795818
Hà Gia Kính <giakinh2000@gmail.com>
2023-03-03 18:47:40 +0700

    fix bug

--- Assets/Scripts/Enemy/EnemySnake.cs
+++ Assets/Scripts/Enemy/EnemySnake.cs
@@ -9,7 +9,6 @@ public class EnemySnake : MonoBehaviour
     private float damage = 0.05f;
     private float currentHealth = 1.5f;
     [SerializeField]
-    public GameObject healthBar;
     private Healbar healbar;
 
     // Start is called before the first frame update
@@ -17,7 +16,6 @@ public class EnemySnake : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         animator.SetFloat("Health", currentHealth);
-        healbar = healthBar.gameObject.GetComponent<Healbar>();
     }
 
     private void Update()
@@ -29,7 +27,7 @@ public class EnemySnake : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            healthBar.SetActive(true);
+            healbar.gameObject.SetActive(true);
             Quaternion rotation = collision.gameObject.transform.rotation;
             gameObject.transform.rotation = rotation;
             Debug.Log(currentHealth);
