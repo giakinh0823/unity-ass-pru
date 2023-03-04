50cfaa8709a2e62ec5896de756f8ea4cef415c37
Hà Gia Kính <giakinh2000@gmail.com>
2023-03-04 17:16:29 +0700

    gun success

---
+++ Assets/ShootController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
+    public float bulletSpeed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && gameObject.activeSelf)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.right * bulletSpeed);
+    }
+}
