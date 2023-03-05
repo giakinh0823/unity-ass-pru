8882f3c011127d2ade4ccd10654306cc68ba130e
Hà Gia Kính <giakinh2000@gmail.com>
2023-03-05 13:25:06 +0700

    animation explosion bullet trigger with ground

--- Assets/Scripts/Player/BulletController.cs
+++ Assets/Scripts/Player/BulletController.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    [SerializeField]
+    public ExplosionBulletGround explosionBulletGround;
+
     void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(explosionBulletGround, transform.position, Quaternion.identity);
     }
 }
