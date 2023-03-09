8abf951b7eccfa3402cee0e1f3c4cac8a69b4d79
Hiếuhihi <84844287+hieuntm2611@users.noreply.github.com>
2023-03-10 01:44:25 +0700

    Add 3 another button and add function

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -4,18 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
     }
     public void SettingGame()
     {
-        SceneManager.LoadScene("Game");
+        //SceneManager.LoadScene("Game");
     }
     public void HighestScore()
     {
-        SceneManager.LoadScene("Game");
+        //SceneManager.LoadScene("Game");
     }
     public void Exit()
     {
