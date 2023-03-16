83a8cc084951269824f2830a4841400ab9e92e00
Nguyen Truong Minh Hieu <hieu261101@gmail.com>
2023-03-17 04:48:16 +0700

    add screen manager to manage screens and popups

---
+++ Assets/Scripts/ScreenManager/Popups/SettingPopup.cs
@@ -0,0 +1,21 @@
+﻿namespace ScreenManager.Popups
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class SettingPopup : BasePopup
+    {
+        [SerializeField] private Slider soundSlider;
+
+        public override void Init()
+        {
+            base.Init();
+            this.soundSlider.onValueChanged.AddListener(OnSoundSliderChangeValue);
+        }
+
+        private static void OnSoundSliderChangeValue(float value)
+        {
+            AudioListener.volume = value;
+        }
+    }
+}
