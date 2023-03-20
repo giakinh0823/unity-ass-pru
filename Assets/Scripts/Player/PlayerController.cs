59646457bf5d3a92f6f1fb9eac2cf37edb679281
anhntl0308 <anhntl0308@gmail.com>
2023-03-20 17:15:31 +0700

    player controller update player local data

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Model;
 using ScreenManager.Popups;
 using ScreenManager.Screens;
 using UnityEngine;
@@ -29,7 +30,11 @@ public class PlayerController : MonoBehaviour
     public int ReviveTime
     {
         get => this.reviveTime;
-        set => this.gameplayScreen.ReviveTime = this.reviveTime = value;
+        set
+        {
+            this.gameplayScreen.ReviveTime = this.reviveTime = value;
+            this.UpdatePlayerLocalData();
+        }
     }
 
     public int CurrentHealth
@@ -57,6 +62,12 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         updateWeapon();
+    }
+
+    public void UpdatePlayerLocalData()
+    {
+        PlayerLocalData.Instance.CurrentPlayerReviveTime = this.ReviveTime;
+        PlayerLocalData.Instance.Save();
     }
 
     public void TakeDamage(int damage)
