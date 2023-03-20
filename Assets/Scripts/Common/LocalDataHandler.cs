7b31ca6332ec6ac3e30a28e56e9fcae57b3dc077
anhntl0308 <anhntl0308@gmail.com>
2023-03-20 17:11:56 +0700

    add player local data

--- Assets/Scripts/Common/LocalDataHandler.cs
+++ Assets/Scripts/Common/LocalDataHandler.cs
@@ -1,27 +1,56 @@
 namespace Common
 {
+    using System;
     using UnityEngine;
 
     public static class LocalDataHandler
     {
         private const string LocalDataPrefix = "LocalData-";
 
-        public static T LoadData<T>()
+        public static bool LoadData<T>(ref T outData)
         {
             var rawData = PlayerPrefs.GetString($"{LocalDataPrefix}{typeof(T).Name}");
 
             if (string.IsNullOrEmpty(rawData))
             {
-                return default;
+                return false;
             }
 
-            return JsonUtility.FromJson<T>(rawData);
+            JsonUtility.FromJsonOverwrite(rawData, outData);
+            return true;
         }
 
         public static void SaveData<T>(T data)
         {
             var rawData = JsonUtility.ToJson(data);
             PlayerPrefs.SetString($"{LocalDataPrefix}{typeof(T).Name}", rawData);
+        }
+    }
+
+    public abstract class BaseLocalData<T> where T : BaseLocalData<T>, new()
+    {
+        public static T Instance { get; set; } = new();
+
+        protected BaseLocalData()
+        {
+            this.Load();
+        }
+
+        public void Save()
+        {
+            LocalDataHandler.SaveData(this);
+        }
+
+        public void Load()
+        {
+            var data = this;
+            if (LocalDataHandler.LoadData(ref data)) return;
+            this.Init();
+            this.Save();
+        }
+
+        public virtual void Init()
+        {
         }
     }
 }
