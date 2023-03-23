b20a3eb7fd1ea7d66dd497f3babbd0bb6e3039b5
Do Nhat Duc <duc21102001@gmail.com>
2023-03-23 10:15:17 +0700

    addSoundForEnemySnake

--- Assets/Scripts/Player/HitController.cs
+++ Assets/Scripts/Player/HitController.cs
@@ -236,7 +236,7 @@ public class HitController : MonoBehaviour
                             enemySnake.transform.localScale = new Vector3(0.7990404f, 0.824f, 1);
                         }
                     }
-
+                    enemySnake.PlaySound();
                     enemySnake.currentHealth -= dameKnifePlayerAttackEnemy;
                 }
                 else if (go.layer == 20)
