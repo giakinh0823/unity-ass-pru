4703af2af06bb072afb8f4ee808bd0f76aa034a4
anhntl0308 <anhntl0308@gmail.com>
2023-03-23 11:05:13 +0700

    update

---
+++ Assets/Scripts/Constant/StateWeaponConstant.cs
@@ -0,0 +1,9 @@
+﻿namespace Constant
+{
+    public class StateWeaponConstant
+    {
+        public static readonly int HAND = 0;
+        public static readonly int KNIFE = 1;
+        public static readonly int GUN = 2;
+    }
+}
