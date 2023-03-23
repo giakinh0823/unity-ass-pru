1aa7fe1b28d84a1d4325e4ec9d2822d9c9763534
giakinh0823 <giakinh2000@gmail.com>
2023-03-23 22:42:27 +0700

    update background

---
+++ Assets/Scripts/Camera/RandomeColorBackground.cs
@@ -0,0 +1,13 @@
+﻿using Constant;
+using UnityEngine;
+
+public class RandomeColorBackground : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = ColorBackgroundConstant.GetRandomColor();
+    }
+}
---
+++ Assets/Scripts/Constant/ColorBackgroundConstant.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Constant
+{
+    public class ColorBackgroundConstant
+    {
+        public static readonly string[] ColorBackground = new string[]
+        {
+            "#3F3F3F",
+            "#BC2A53",
+            "#019D82",
+            "#009852",
+            "#EC7700",
+            "#F500FF",
+            "#EF2A34"
+        };
+
+        public static Color GetRandomColor()
+        {
+            // random color
+            int rand = Random.Range(0, ColorBackground.Length);
+            ColorUtility.TryParseHtmlString(ColorBackground[rand], out var color);
+            return color;
+        }
+    }
+}
